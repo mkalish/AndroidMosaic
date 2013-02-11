43fc31d55819cdb305768e7564d82c0a43e92d45
Mark Pierotti <mark.pierotti@appian.com>
2013-02-11 15:24:36 -0500

    Changed Preprocessor.analyze() to be bulk

--- src/com/webb/androidmosaic/generator/Preprocessor.java
+++ src/com/webb/androidmosaic/generator/Preprocessor.java
@@ -1,8 +1,10 @@
 package com.webb.androidmosaic.generator;
 
+import java.util.List;
+
 import android.graphics.Bitmap;
 
 public interface Preprocessor {
 	//take images, convert to simplified type for generator to work with.
-	AnalyzedImage analyze(Bitmap bitmap);
+	AnalyzedImage analyze(List<Bitmap> bitmaps);
 }
--- src/com/webb/androidmosaic/generator/PreprocessorImpl.java
+++ src/com/webb/androidmosaic/generator/PreprocessorImpl.java
@@ -1,10 +1,12 @@
 package com.webb.androidmosaic.generator;
 
+import java.util.List;
+
 import android.graphics.Bitmap;
 
 public class PreprocessorImpl implements Preprocessor {
 
-	public AnalyzedImage analyze(Bitmap bitmap) {
+	public AnalyzedImage analyze(List<Bitmap> bitmaps) {
 		// TODO Auto-generated method stub
 		return null;
 	}
