fe15ee1cc00b817a7b08c7a6af1b9ab60e310c3b
Mark Pierotti <mark.pierotti@appian.com>
2013-02-11 15:21:27 -0500

    Slight comment updates in ColorSpaceUtils

--- src/com/webb/androidmosaic/generator/ColorSpaceUtils.java
+++ src/com/webb/androidmosaic/generator/ColorSpaceUtils.java
@@ -2,12 +2,14 @@ package com.webb.androidmosaic.generator;
 
 public class ColorSpaceUtils {
 	
+	//CIE1976
 	public static LABValue RGBToLAB(int r, int g, int b){
 		//rgb to xyz, xyz to lab
 		return new LABValue(r,g,b);//TODO, these should be the correct l,a,b values instead
 	}
 	
-	public static void LABColorDistance(Object firstLABValue, Object secondLABValue){
-		
+	//CIE94 color difference
+	public static int LABColorDistance(Object firstLABValue, Object secondLABValue){
+		return 0;
 	}
 }
