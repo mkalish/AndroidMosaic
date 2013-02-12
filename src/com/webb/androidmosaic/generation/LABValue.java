8194569268a34a9c58e3b233ab8b9101a31492af
Mark Pierotti <mark.pierotti@appian.com>
2013-02-12 14:11:26 -0500

    Added getters to LABValue

--- src/com/webb/androidmosaic/generation/LABValue.java
+++ src/com/webb/androidmosaic/generation/LABValue.java
@@ -2,7 +2,6 @@ package com.webb.androidmosaic.generation;
 
 public class LABValue {
 	int l;
-
 	int a;
 	int b;
 	
@@ -10,6 +9,15 @@ public class LABValue {
 		this.l=l;
 		this.a=a;
 		this.b=b;
+	}
+	public int getL() {
+		return l;
+	}
+	public int getA() {
+		return a;
+	}
+	public int getB() {
+		return b;
 	}
 	@Override
 	public String toString() {
